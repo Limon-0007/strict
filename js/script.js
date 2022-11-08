f704e0ab1466b0ba2e20f949264ad13fee0a7b48
Limon-0007 <limonahmed3023@gmail.com>
2022-11-08 23:03:56 +0600

    added site resources

--- js/script.js
+++ js/script.js
@@ -2,11 +2,36 @@ new WOW().init();
 
 let navbar = document.getElementById("navBar");
 
-window.addEventListener ("scroll", function () {
-  if (window.pageYOffset > 500) 
-  {
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 500) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 });
+
+// dark mode
+
+// function addDarkmodeWidget() {
+//   new Darkmode().showWidget();
+// }
+// window.addEventListener("load", addDarkmodeWidget);
+
+// const options = {
+//   bottom: "64px", // default: '32px'
+//   right: "unset", // default: '32px'
+//   left: "42px", // default: 'unset'
+//   time: "0.3s", // default: '0.3s'
+//   mixColor: "#fff", // default: '#fff'
+//   backgroundColor: "#fff", // default: '#fff'
+//   buttonColorDark: "#100f2c", // default: '#100f2c'
+//   buttonColorLight: "#fff", // default: '#fff'
+//   saveInCookies: true, // default: true,
+//   label: "🌓", // default: ''
+//   autoMatchOsTheme: true, // default: true
+// };
+
+// let dark = document.getElementsByClassName("darkmode-toggle");
+
+// const darkmode = new Darkmode(options);
+// new Darkmode().showWidget();
